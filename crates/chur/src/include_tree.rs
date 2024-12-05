69a401b4ab325dbcabbb6169a64d36edc8411b35
d3rpp <github@d3rpp.dev>
2024-12-06 10:54:55 +1300

    fix weirdness

--- crates/chur/src/include_tree.rs
+++ crates/chur/src/include_tree.rs
@@ -102,10 +102,12 @@ pub(super) fn include_tree() -> TokenStream {
             if entry.path().is_file() {
                 if let Some(file_name) = entry.path().file_name() {
                     let fn_string = file_name.to_string_lossy();
-                    if fn_string == "__fd.bin" {
-                        fd = Some(entry.path())
+                    if fn_string.starts_with("__") {
+                        if fn_string == "__fd.bin" {
+                            fd = Some(entry.path());
+                        }
                     } else if let Some(ext) = entry.path().extension() {
-                        if ext == "rs" && fn_string != "__pb.rs" {
+                        if ext == "rs" {
                             return Some(
                                 entry
                                     .file_name()
